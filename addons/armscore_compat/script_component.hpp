83d1701be409e5217843dad382423d28a28586df
Tapawingo <tapawingogamer@gmail.com>
2023-05-04 14:02:57 +0200

    Moved armscore_compat to CCA3

--- addons/armscore_compat/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT armscore_compat
-#define COMPONENT_BEAUTIFIED ARMScore Compatability
-#include "\z\ccm\addons\main\script_mod.hpp"
-
-#include "\z\ccm\addons\main\script_macros.hpp"
